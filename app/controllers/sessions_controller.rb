0084826bedc0847a078ab46df7784c93f70cfd39
Donald Ali <donaldali@yahoo.com>
2014-06-04 14:09:06 -0400

    Finish Chapter 8 exercises

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
 	def new
 	end
 
-	def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+  def create
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
       sign_in user
       redirect_to user
     else
@@ -13,6 +13,17 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
+
+ # def create
+ #    user = User.find_by(email: params[:session][:email].downcase)
+ #    if user && user.authenticate(params[:session][:password])
+ #      sign_in user
+ #      redirect_to user
+ #    else
+ #      flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+ #      render 'new'
+ #    end
+ #  end
 
 	def destroy
 		sign_out
